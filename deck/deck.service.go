735089ce7549fb9b301442e1c3844ce485884cf4
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-09 12:02:15 -0300

    Update deck.service.go

--- deck/deck.service.go
+++ deck/deck.service.go
@@ -50,11 +50,10 @@ func (deck deck) Printer() {
 	}
 }
 
-func (d deck) Shuffle() deck {
+func (d deck) Shuffle() {
 	rand.Seed(time.Now().UnixNano())
 	for i := len(d) - 1; i > 0; i-- {
 		j := rand.Intn(i + 1)
 		d[i], d[j] = d[j], d[i]
 	}
-	return d
 }
