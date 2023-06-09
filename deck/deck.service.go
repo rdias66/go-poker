0f2984e6142bf0167f10da675bd943f1e7ec28b6
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-09 11:38:24 -0300

    Create deck.service.go
    Separating type declarations and associated functions , step 2

---
+++ deck/deck.service.go
@@ -0,0 +1,44 @@
+package deck
+
+func SuitBuilder(suitName string) suit {
+	newSuit := suit{}
+	for i := 1; i <= 13; i++ {
+		if i == 1 {
+			var newCard card = NewCard("Ace of ", suitName)
+			newSuit = append(newSuit, newCard)
+		} else if i == 11 {
+			var newCard card = NewCard("Jack of ", suitName)
+			newSuit = append(newSuit, newCard)
+		} else if i == 12 {
+			var newCard card = NewCard("Queen of ", suitName)
+			newSuit = append(newSuit, newCard)
+		} else if i == 13 {
+			var newCard card = NewCard("King of ", suitName)
+			newSuit = append(newSuit, newCard)
+		} else {
+			var newCard card = NewCard(fmt.Sprint(i), suitName)
+			newSuit = append(newSuit, newCard)
+		}
+	}
+	return newSuit
+}
+
+func NewCard(value string, suit string) card {
+	return card{value, suit}
+}
+
+func DeckBuilder(suits []suit) deck {
+	newDeck := deck{}
+	for _, s := range suits {
+		for _, c := range s {
+			newDeck = append(newDeck, c)
+		}
+	}
+	return newDeck
+}
+
+func (deck deck) DeckPrinter() {
+	for _, d := range deck {
+		fmt.Println(d)
+	}
+}
