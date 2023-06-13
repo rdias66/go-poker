9883453aa7e7cc25a48ad9af27f57fd22bae5902
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-13 11:28:04 -0300

    Create dealer.service.go

---
+++ dealer/dealer.service.go
@@ -0,0 +1,49 @@
+package dealer
+
+func newRound(fullDeck deck.Deck, ps []player.Player){
+	return Round{FullDeck : fullDeck , Players : ps}
+}
+
+func  (Round) DrawCards(d deck.Deck, ps []player.Player){
+	for _, p := range ps {
+		p.Hand, d := DrawCards(d, 2)
+	}
+	r := newRound(d, ps)
+	return r
+}
+
+func (r Round) DealFlop(r Round) {
+	burn, deck := DrawCards(r.FullDeck, 1)
+	r.Burns := append(r.Burns, burn)
+	flop, deck := DrawCards(r.FullDeck, 3)
+	r.Flop := append(r.Flop, flop)
+	return r
+}
+
+func (r Round) DealTurn(r Round) {
+	burn, deck := DrawCards(r.FullDeck, 1)
+	r.Burns := append(r.Burns, burn)
+	turn, deck := DrawCards(r.FullDeck, 1)
+	r.Turn := append(r.Turn, turn)
+	return r
+}
+
+func (r Round) DealRiver(r Round) {
+	burn, deck := DrawCards(r.FullDeck, 1)
+	r.Burns := append(r.Burns, burn)
+	river, deck := DrawCards(r.FullDeck, 1)
+	r.River := append(r.River, river)
+	return r
+}
+
+func DrawCards(d deck.Deck, numCards int) (deck.Deck, deck.Deck){
+	deckSize := len(d)
+	if numCards > deckSize {
+		numCards = deckSize // If the requested number of cards exceeds the deck size, draw all the remaining cards
+	}
+
+	draw := deck[deckSize-numCards:] // Extract the last 'numCards' from the deck
+	deck = deck[:deckSize-numCards]  // Remove the drawn cards from the deck
+
+	return draw, deck
+}
