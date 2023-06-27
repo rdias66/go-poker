3378fdb1e9bedc649a1eba11da78011ecdc7f25f
Rodrigo Dias <88790315+rdias66@users.noreply.github.com>
2023-06-27 11:33:16 -0300

    Update player.models.go

--- player/player.models.go
+++ player/player.models.go
@@ -1,16 +1,13 @@
 package player
 
+import "go-poker/rules"
+
 type Player struct {
     Hand []deck.Card
     FullHand []deck.Card
     Id string
     Name string
-    Result *FinalHand
-}
-
-type FinalHand struct {
-    Hand[]deck.Card
-    HandResult string
+    HandRank HandRank
 }
 
 func (p Player) ShowHand() []deck.Card {
